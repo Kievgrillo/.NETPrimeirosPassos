6375eb728dd054fe89003b4ba6d5ae8e2d293fbd
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-18 18:10:17 -0300

    Add files via upload

---
+++ Atividade6/Atividade6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace Uri1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            soma = 0.0;
+            cont = 0;
+
+            while (idade >= 0)
+            {
+                soma = soma + idade; //calculo para saber a idade total
+                cont = cont + 1;    //consegue computador todas as idades que o programa digitar
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // para fazer o while rodar novamente
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
