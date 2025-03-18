5978a866c6afdf9d2d1f3b2272b6e17bae653f8a
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-18 18:08:45 -0300

    Add files via upload

---
+++ ImplementandoFor/ImplementandoFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ImplementadoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, X, Soma;
+
+            N = int.Parse(Console.ReadLine());
+            
+            Soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                X = int.Parse(Console.ReadLine());
+                Soma = Soma + X;
+            }
+            Console.WriteLine(Soma);
+            Console.ReadLine();
+        }
+    }
+}
