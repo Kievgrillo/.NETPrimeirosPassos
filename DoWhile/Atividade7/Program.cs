a49617ef57e1206ab55474e8bd402b8a133cd270
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-19 13:43:40 -0300

    Add files via upload

---
+++ DoWhile/Atividade7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            char repetir;
+
+            do
+            {
+                Console.WriteLine("Digite a temperatura em Celsius:");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5 + 32;
+                Console.WriteLine("Equivalente em Fahrenheit:" + F.ToString("F1"), CultureInfo.InvariantCulture);
+                Console.WriteLine("Deseja repetir (s/n)?");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+
+            Console.ReadLine();
+        }
+    }
+}
